e7902f131fd1037f0901755ece1bde3fe9e8c07e
abnobery <noirik09@gmail.com>
2019-02-28 20:27:05 +0200

    basic structure

---
+++ Classes/Photo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleHashCode2019.Classes
+{
+    class Photo
+    {
+        public enum ePhotoOrientation { H = 1, V = 2 };
+
+        public ePhotoOrientation Orientation;
+        public List<string> Tags;
+    }
+}
---
+++ Classes/Slide.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleHashCode2019.Classes {
+class Slide
+    {
+        List<Photo> photos;
+    }
+}
