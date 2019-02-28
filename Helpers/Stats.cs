574364118736143831fdbc2470beeb9f6e89ad55
manwatermelon <manwatermelon@Alexanders-MacBook-Pro-Home.local>
2019-02-28 21:29:33 +0200

    Fixed

--- Helpers/Stats.cs
+++ Helpers/Stats.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 namespace GoogleHashCode2019.Helpers
 {
-    public class Difference
+    public class Stats
     {
         public int Common { get; set; }
         public int MinDiff { get; set; }
-        public Difference()
+        public Stats()
         {
         }
     }
