1dfab06cc5f431e7737e4403201717afd4fa53cc
manwatermelon <manwatermelon@Alexanders-MacBook-Pro-Home.local>
2019-02-28 21:17:03 +0200

    Added helper

---
+++ Helpers/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace GoogleHashCode2019.Helpers
+{
+    public static class Helper
+    {
+        public static int GetIntersectWeight(List<string> l1, List<string> l2)
+        {
+            int result = 0;
+            result = l1.Intersect(l2).Count();
+            return result;
+        }
+    }
+}
